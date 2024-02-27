7c5dd318f541b222d4e4b218762d2d323959bae8
yongun2 <djkye0001@naver.com>
2024-02-27 22:02:18 +0900

    project initialization

---
+++ src/main/java/pcrc/alcha/AlchaApplication.java
@@ -0,0 +1,13 @@
+package pcrc.alcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pcrc/alcha/AlchaApplicationTests.java
@@ -0,0 +1,13 @@
+package pcrc.alcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
