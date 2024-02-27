fc21a0c2787631dbc66c982f4054079f95af00c0
yongun2 <djkye0001@naver.com>
2024-02-27 22:05:34 +0900

    application.properties 제외

--- src/main/java/pcrc/alcha/AlchaApplication.java
+++ src/main/java/pcrc/alcha/AlchaApplication.java
@@ -2,7 +2,9 @@ package pcrc.alcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity(debug = true)
 @SpringBootApplication
 public class AlchaApplication {
 
