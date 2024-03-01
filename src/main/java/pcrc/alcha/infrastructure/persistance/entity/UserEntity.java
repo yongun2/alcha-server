5fa7f78222523c24de1e273a8ffdb1b35cdfb7c0
yongun2 <djkye0001@naver.com>
2024-03-01 17:23:33 +0900

    [FEAT] 유저 관련 Entity, VO, DTO

---
+++ src/main/java/pcrc/alcha/infrastructure/persistance/entity/UserEntity.java
@@ -0,0 +1,61 @@
+package pcrc.alcha.infrastructure.persistance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pcrc.alcha.application.domain.auth.RefreshToken;
+import pcrc.alcha.application.domain.auth.User;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String profileImgUrl;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "refresh_token_id")
+    private RefreshTokenEntity refreshTokenEntity;
+
+    public UserEntity(User user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.profileImgUrl = user.getProfileImgUrl();
+    }
+
+    public UserEntity(User user, RefreshToken refreshToken) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.profileImgUrl = user.getProfileImgUrl();
+        this.refreshTokenEntity = new RefreshTokenEntity(refreshToken);
+    }
+
+    public User toUser() {
+        return User.builder()
+                .id(this.id)
+                .username(this.username)
+                .nickname(this.nickname)
+                .profileImgUrl(this.profileImgUrl)
+                .build();
+    }
+
+    public RefreshToken toRefreshToken() {
+        return RefreshToken.builder()
+                .id(refreshTokenEntity.getId())
+                .token(refreshTokenEntity.getToken())
+                .build();
+    }
+}
