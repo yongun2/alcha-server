c82eb997ddaff5216985a2abfe159ae57b39ddf7
yongun2 <djkye0001@naver.com>
2024-03-01 17:22:57 +0900

    [FEAT] 유저 로그인, 중복확인 기능

---
+++ src/main/java/pcrc/alcha/application/service/user/UserService.java
@@ -0,0 +1,117 @@
+package pcrc.alcha.application.service.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pcrc.alcha.application.domain.auth.User;
+import pcrc.alcha.application.service.image.ImageService;
+import pcrc.alcha.exception.AlchaException;
+import pcrc.alcha.exception.MessageType;
+import pcrc.alcha.infrastructure.persistance.entity.UserEntity;
+import pcrc.alcha.infrastructure.persistance.repository.auth.UserRepository;
+
+import java.util.regex.Pattern;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserOperationUseCase, UserReadUseCase {
+
+    private final ImageService imageService;
+    private final UserRepository repository;
+
+
+    private final String VALID_USERNAME_PATTERN = "^[a-z0-9_-]{2,16}$";
+    private final String VALID_PASSWORD_PATTERN = "^(?=.*[a-zA-Z])" +
+            "(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\",./<>?])" +
+            "[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\",./<>?]{8,18}$";
+
+    @Override
+    @Transactional
+    public FindUserResult register(UserCreateCommand command) {
+
+        validateCommand(command);
+
+        String imageSaveUrl = imageService.save(command.profileImgBase64());
+
+        User user = User.builder()
+                .username(command.username())
+                .password(command.password())
+                .nickname(command.nickname())
+                .profileImgUrl(imageSaveUrl)
+                .build();
+
+        return FindUserResult.findByUser(
+                repository.save(new UserEntity(user)).toUser()
+        );
+    }
+
+    @Override
+    public FindUserResult getUserByUsername(UserFindQuery query) {
+        return FindUserResult.findByUser(
+                repository.findUserEntityByUsername(query.username())
+                        .orElseThrow(() -> new AlchaException(MessageType.USER_NOT_FOUND))
+                        .toUser()
+        );
+    }
+
+    @Override
+    public FindUserResult getUserByNickname(UserFindQuery query) {
+        return FindUserResult.findByUser(
+                repository.findUserEntityByNickname(query.nickname())
+                        .orElseThrow(() -> new AlchaException(MessageType.USER_NOT_FOUND))
+                        .toUser()
+        );
+    }
+
+    @Override
+    public FindUserResult checkDuplicate(UserFindQuery query) {
+        if (query.username() != null) {
+            repository.findUserEntityByUsername(query.username())
+                    .ifPresent((userEntity) -> {
+                        throw new AlchaException(MessageType.DUPLICATED_USERNAME);
+                    });
+            return FindUserResult.findByUsername(query.username());
+        }
+
+        if (query.nickname() != null) {
+            repository.findUserEntityByNickname(query.nickname())
+                    .ifPresent((userEntity) -> {
+                        throw new AlchaException(MessageType.DUPLICATED_USERNAME);
+                    });
+            return FindUserResult.findByNickname(query.nickname());
+        }
+
+        throw new AlchaException(MessageType.INTERNAL_SERVER_ERROR);
+    }
+
+    private void validateCommand(UserCreateCommand command) {
+        if (!isValidUsername(command.username())) {
+            throw new AlchaException(MessageType.BAD_USERNAME_PATTERN);
+        }
+        if (!isValidPassword(command.password())) {
+            throw new AlchaException(MessageType.BAD_PASSWORD_PATTERN);
+        }
+        if (!isValidNickname(command.nickname())) {
+            throw new AlchaException(MessageType.BAD_NICKNAME_PATTERN);
+        }
+    }
+    private boolean isValidUsername(String username) {
+        return Pattern.compile(VALID_USERNAME_PATTERN)
+                .matcher(username)
+                .matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        return Pattern.compile(VALID_PASSWORD_PATTERN)
+                .matcher(password)
+                .matches();
+    }
+
+    private boolean isValidNickname(String nickname) {
+        return !nickname.isBlank() && nickname.length() >= 2 && nickname.length() <= 12;
+    }
+
+
+}
